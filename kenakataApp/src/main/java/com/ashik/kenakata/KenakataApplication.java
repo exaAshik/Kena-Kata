a838841c60ea4b8a8a5e8bfc0e39077a272a8c95
Md Ashikur Rahman <ashikurrahman@Mds-MacBook-Air.local>
2024-10-27 14:17:27 +0600

    Initial test commit

---
+++ kenakataApp/src/main/java/com/ashik/kenakata/KenakataApplication.java
@@ -0,0 +1,13 @@
+package com.ashik.kenakata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenakataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenakataApplication.class, args);
+	}
+
+}
---
+++ kenakataApp/src/test/java/com/ashik/kenakata/KenakataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashik.kenakata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenakataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
